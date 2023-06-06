dfd44f935968615d3d5abdfdfd784baab5959a63
Noelia Carosella <72532638+honeybadger2788@users.noreply.github.com>
2023-06-06 19:13:55 -0300

    feature: di implemented (#7)

--- app/src/main/java/com/girlify/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/girlify/rockpaperscissors/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -18,7 +19,9 @@ import com.girlify.rockpaperscissors.game.ui.home.HomeScreen
 import com.girlify.rockpaperscissors.game.ui.multiPlayer.MultiPlayerScreen
 import com.girlify.rockpaperscissors.game.ui.singlePlayer.GameScreen
 import com.girlify.rockpaperscissors.ui.theme.RockPaperScissorsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,46 +32,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navigationController: NavHostController = rememberNavController()
-
-                    NavHost(
-                        navController = navigationController,
-                        startDestination = Routes.Home.route
-                    ) {
-                        composable(Routes.Home.route) {
-                            HomeScreen(
-                                {
-                                    navigationController.navigate(Routes.SinglePlayer.route) {
-                                        popUpToId
-                                    }
-                                },
-                                {
-                                    navigationController.navigate(
-                                        Routes.MultiPlayer.createRoute(
-                                            username = it
-                                        )
-                                    ) {
-                                        popUpToId
-                                    }
-                                }
-                            )
-                        }
-                        composable(Routes.SinglePlayer.route) {
-                            GameScreen()
-                        }
-                        composable(
-                            Routes.MultiPlayer.route,
-                            arguments = listOf(navArgument("username") {
-                                type = NavType.StringType
-                            })
-                        ) { backStackEntry ->
-                            MultiPlayerScreen(
-                                backStackEntry.arguments?.getString("username") ?: ""
-                            )
-                        }
-                    }
+                    MyNavigation()
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun MyNavigation(){
+    val navigationController: NavHostController = rememberNavController()
+    NavHost(
+        navController = navigationController,
+        startDestination = Routes.Home.route
+    ) {
+        composable(Routes.Home.route) {
+            HomeScreen(
+                {
+                    navigationController.navigate(Routes.SinglePlayer.route) {
+                        popUpToId
+                    }
+                },
+                {
+                    navigationController.navigate(
+                        Routes.MultiPlayer.createRoute(
+                            username = it
+                        )
+                    ) {
+                        popUpToId
+                    }
+                }
+            )
+        }
+
+        composable(Routes.SinglePlayer.route) {
+            GameScreen()
+        }
+
+        composable(
+            Routes.MultiPlayer.route,
+            arguments = listOf(navArgument("username") {
+                type = NavType.StringType
+            })
+        ) { backStackEntry ->
+            MultiPlayerScreen(
+                backStackEntry.arguments?.getString("username") ?: ""
+            )
         }
     }
 }
--- app/src/main/java/com/girlify/rockpaperscissors/game/data/network/FirebaseClient.kt
+++ app/src/main/java/com/girlify/rockpaperscissors/game/data/network/FirebaseClient.kt
@@ -1,71 +1,12 @@
 package com.girlify.rockpaperscissors.game.data.network
 
-import android.util.Log
-import com.girlify.rockpaperscissors.game.data.response.GameModel
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+import javax.inject.Singleton
 
 
-class FirebaseClient {
-    private val db = Firebase.database.reference
-    private val gameRef = db.child("games")
-
-    fun setGame(gameId: String) {
-        gameRef.child(gameId)
-    }
-
-    fun gameListener(gameId: String): Flow<GameModel?>  {
-        val gameDataFlow = MutableStateFlow<GameModel?>(null)
-        gameRef.child(gameId).addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val gameModel = dataSnapshot.getValue(GameModel::class.java)
-                gameDataFlow.value = gameModel
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w("NOE", "loadPost:onCancelled", databaseError.toException())
-            }
-        })
-        return gameDataFlow
-    }
-
-    suspend fun setPlayer(gameId: String, player: Int, username: String) {
-        gameRef.child(gameId).updateChildren(
-            mapOf(
-                if (player == 1) "player1" to username else "player2" to username
-            )
-        ).await()
-    }
-
-    suspend fun makeMove(gameId: String, player: Int, playerMove: String) {
-        gameRef.child(gameId).updateChildren(
-            mapOf(
-                if (player == 1) "player1Choice" to playerMove else "player2Choice" to playerMove
-            )
-        ).await()
-    }
-
-    suspend fun restartGame(gameId: String) {
-        gameRef.child(gameId).updateChildren(
-            mapOf(
-                "player1Choice" to "",
-                "player2Choice" to ""
-            )
-        ).await()
-    }
-
-    suspend fun deleteGame(gameId: String) {
-        gameRef.child(gameId).removeValue().await()
-    }
-    suspend fun getGame(gameId: String): Boolean {
-        val response = gameRef.child(gameId).get().await()
-        return response.value != null
-    }
+@Singleton
+class FirebaseClient @Inject constructor() {
+    val db = Firebase.database.reference
 }
