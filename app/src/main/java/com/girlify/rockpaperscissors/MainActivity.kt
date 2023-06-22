75683dd8566b7124ec67203c1f75bff6daa4d92c
honeybadger2788 <noe.carosella@gmail.com>
2023-06-22 18:13:43 -0300

    feature: splash screen and app icon implemented

--- app/src/main/java/com/girlify/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/girlify/rockpaperscissors/MainActivity.kt
@@ -3,11 +3,13 @@ package com.girlify.rockpaperscissors
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -18,13 +20,17 @@ import com.girlify.rockpaperscissors.game.core.model.Routes
 import com.girlify.rockpaperscissors.game.ui.home.HomeScreen
 import com.girlify.rockpaperscissors.game.ui.multiPlayer.MultiPlayerScreen
 import com.girlify.rockpaperscissors.game.ui.singlePlayer.SinglePlayerScreen
+import com.girlify.rockpaperscissors.game.ui.splashScreen.SplashViewModel
 import com.girlify.rockpaperscissors.ui.theme.RockPaperScissorsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val splashViewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition{splashViewModel.isLoading.value}
         setContent {
             RockPaperScissorsTheme {
                 // A surface container using the 'background' color from the theme
